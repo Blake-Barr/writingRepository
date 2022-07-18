4c00ce2254f7f8fef8a2501d0bcbae1122961ed0
Blakedasnake727 <barrblake555@gmail.com>
2022-07-17 21:27:09 -0400

    test

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by barrb on 7/17/2022.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,16 @@
+//
+// Created by barrb on 7/17/2022.
+//
+
+#ifndef WRITINGREPOSITORY_TEST_H
+#define WRITINGREPOSITORY_TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //WRITINGREPOSITORY_TEST_H
